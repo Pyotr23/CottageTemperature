74d2aa8934d158e5c85e09d06024292ee2cbe61a
Pyotr <2chilavert3@mail.ru>
2020-10-11 22:45:41 +0300

    Рефакторинг, загорается лампочка при превышении порога влажности в 30%.

--- da4a-temp.ino
+++ da4a-temp.ino
@@ -1,18 +1,41 @@
 #include <dht.h>
 
 dht DHT;
+const int HIGH_ALARM_HUMIDITY_TRESHOLD = 30;
 
 #define DHT11_PIN 7
+#define ALARM_PIN 8
 
 void setup(){
+  pinMode(ALARM_PIN, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop(){
   int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println((int)DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println((int)DHT.humidity);
+  int temperature = (int)DHT.temperature;
+  int humidity = (int)DHT.humidity;
+  printDhtParameters(temperature, humidity);   
+  boolean alarm = isAlarm(humidity, HIGH_ALARM_HUMIDITY_TRESHOLD);
+  digitalWrite(ALARM_PIN, alarm);  
   delay(5000);
+}
+
+boolean isAlarm(int sensorValue, int treshold){
+  return sensorValue >= treshold;
+}
+
+void printTemperature(int temperature){
+  Serial.print("Temperature = ");
+  Serial.println(temperature);  
+}
+
+void printHumidity(int humidity){
+  Serial.print("Humidity = ");
+  Serial.println(humidity);  
+}
+
+void printDhtParameters(int temperature, int humidity){
+  printTemperature(temperature);
+  printHumidity(humidity);
 }
