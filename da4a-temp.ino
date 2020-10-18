2feff1b79e0cb26ce915da1a5fc44a3a30b678de
Pyotr <2chilavert3@mail.ru>
2020-10-18 16:50:18 +0300

    Отображаются в лаконичном виде запросы к SIM800L и ответы.

--- da4a-temp.ino
+++ da4a-temp.ino
@@ -12,31 +12,25 @@ SoftwareSerial SIM800(RX_PIN, TX_PIN);
 const int HIGH_ALARM_HUMIDITY_TRESHOLD = 30;
 const int HIGH_ARARM_TEMPERATURE_TRESHOLD = 27;
 const int START_DELAY_IN_MS = 1000;
+const String DELIMITER_BETWEEN_REQUEST_AND_RESPONSE = String(char(13)) + String(char(13)) + String(char(10));
 
 boolean isSent = false;
 long lastcmd = millis();
 
 void setup(){
-  Serial.begin(9600);               // Скорость обмена данными с компьютером
+  Serial.begin(9600);               
   SIM800.begin(9600); 
-  delay(5000); 
+  delay(1000); 
   Serial.println("Start!");
-  while(!SIM800.available()){             // Зацикливаем и ждем инициализацию SIM800L
-    SIM800.println("AT");                  // Отправка команды AT
-    delay(1000);                             // Пауза
-    Serial.println("Connecting...");         // Печатаем текст
-  }
-  Serial.println("Connected!");            // Печатаем текст
-  SIM800.println("AT+CMGF=1");           // Отправка команды AT+CMGF=1
-  delay(1000);                             // Пауза
-  SIM800.println("AT+CSCS=\"GSM\"");  
-  delay(1000);                             // Пауза 
+  ConnectToSim800l();
+  SendRequestAndPrintResponse("AT+CMGF=1");  
+  SendRequestAndPrintResponse("AT+CSCS=\"GSM\"");  
   // sms("Hello world", "+79296135951");
 }
 
 void loop(){
-  if (SIM800.available())           // Ожидаем прихода данных (ответа) от модема...
-    Serial.write(SIM800.read());    // ...и выводим их в Serial
+  // if (SIM800.available())           // Ожидаем прихода данных (ответа) от модема...
+  //   Serial.write(SIM800.read());    // ...и выводим их в Serial
   // if (Serial.available())           // Ожидаем команды по Serial...
   //   SIM800.write(Serial.read());    // ...и отправляем полученную команду модему
 
@@ -57,4 +51,31 @@ void sms(String text, String phone)
   delay(300);    
   Serial.println("SMS send finish");    
   delay(3000);  
+}
+
+void ConnectToSim800l(){
+  while(!SIM800.available()){             
+    SIM800.println("AT");
+    Serial.println("Connecting...");         
+    delay(1000);    
+  }
+  Serial.println("Connected!");           
+  PrintResponse();  
+}
+
+void SendRequestAndPrintResponse(String request){
+  SIM800.println(request);
+  delay(1000);
+  PrintResponse();
+}
+
+void PrintResponse(){  
+  String response = "";
+  while(SIM800.available()){ 
+    char symbolNumber = SIM800.read();
+    if (symbolNumber != 0)           
+      response += char(symbolNumber); 
+  }  
+  response.replace(DELIMITER_BETWEEN_REQUEST_AND_RESPONSE, ": ");
+  Serial.print(response);    
 }
