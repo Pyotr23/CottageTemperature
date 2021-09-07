923c82b55a566554ac3750017ca74dc6f006d415
Pyotr <2chilavert3@mail.ru>
2021-09-07 23:19:45 +0300

    Добавление логики переключения реле без подключения реле.

--- CottageTemperature.ino
+++ CottageTemperature.ino
@@ -1,20 +1,29 @@
 #include <DHTesp.h>
 
-// вывод для управления датчиком влажности и температуры
+// Вывод для управления датчиком влажности и температуры.
 const uint8_t DHT11_PIN = 2;
-// датчик температуры и влажности DHT
+
+// Значение нижнего температурного порога.
+const int LOW_TEMPERATURE_TRESHOLD = 10;  
+// Значение верхнего температурного порога.
+const int HIGH_TEMPERATURE_TRESHOLD = 30;
+// Версия устройства.
+const String VERSION = "4.0"; 
+
+// Датчик температуры и влажности DHT.
 DHTesp dhtModule;
-// текущее значение температуры
+
+// Текущее значение температуры.
 int temperature;
-// текущее значение влажности
+// Текущее значение влажности.
 int humidity;
 
-char incomingByte;
-int min = 10;
-int max = 30;
-int index;
+// Флаг непрерывного нагрева. Установленные пороги при выставленном флаге игнорируются.
 bool isContiniousHeating = false;
-String command;
+// Флаг нагрева.
+bool isHeating = false;
+// Флаг предыдущего состояния нагрева.
+bool wasHeating = false;
 
 void setup()
 {
@@ -26,31 +35,33 @@ void loop() {
     delay(3000);
 
     if (Serial.available() > 0){
-        command = GetSerialPortText();    
+        String command = GetSerialPortText();    
 
         Serial.println(command);
         Serial.println("----------");
       
+        int index;
+ 
         if (command.startsWith("/min")){
             index = command.indexOf(':');
-            min = command.substring(index + 1).toInt();            
+            LOW_TEMPERATURE_TRESHOLD = command.substring(index + 1).toInt();            
         }            
         else if (command.startsWith("/max")){
             index = command.indexOf(':');
-            max = command.substring(index + 1).toInt();
+            HIGH_TEMPERATURE_TRESHOLD = command.substring(index + 1).toInt();
         }
         else if (command.startsWith("/heat")){
             index = command.indexOf(':');
             isContiniousHeating = command.substring(index + 1).toInt();
         }
         else if (command.startsWith("/info")){
-            max = 23;
+            HIGH_TEMPERATURE_TRESHOLD = 23;
         }
 
         WriteParameters();
 
-        Serial.println(min);
-        Serial.println(max);
+        Serial.println(LOW_TEMPERATURE_TRESHOLD);
+        Serial.println(HIGH_TEMPERATURE_TRESHOLD);
         Serial.println(isContiniousHeating);
         Serial.println(temperature);
         Serial.println(humidity);
@@ -69,9 +80,38 @@ String GetSerialPortText(){
     String text;
     while (Serial.available() > 0) {  
         char incomingByte = Serial.read();            
-        if (incomingByte == 10 || incomingByte == 13)   // выход из метода при достижении символов конца строки
+        if (incomingByte == 10 || incomingByte == 13)   // символы конца строки пропускаются
             continue;
         text += incomingByte;
     }   
     return text;   
+}
+
+// Уровень сигнала, подаваемого на вход реле, исходя из значения температуры и её порогов.
+// Если температура выше (или равна) значения верхнего порога, то подаётся 1 и KY-019 размыкается.
+// Если температура стала ниже (или равна) значения нижнего порога, то подаётся 0 и KY-019 замыкается.
+boolean IsOnRelay(){
+    if (isContiniousHeating){
+        isHeating = false;
+        return false;
+    }
+
+
+    if (temperature >= HIGH_TEMPERATURE_TRESHOLD){
+    wasHeating = isHeating;
+    isHeating = false; 
+    // if (wasHeating != isHeating)    // сообщение, что прекратился нагрев       
+    //   SendEmail(GetMessageText(currentMode));  
+    return true; 
+    } 
+
+    if (temperature <= LOW_TEMPERATURE_TRESHOLD){
+    wasHeating = isHeating;
+    isHeating = true; 
+    // if (wasHeating != isHeating)       
+    //   SendEmail(GetMessageText(currentMode));   // сообщение, что начался нагрев  
+    return false;   
+    }
+
+    return !isHeating;
 }
